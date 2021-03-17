90cc31b1d7c601c087f8cc6e22a4bce5846174a5
iiTONELOC <anthonytropeano@me.com>
2021-03-16 21:47:36 -0400

    added a lot of stuff

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,15 @@
-
-// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 require('dotenv').config()
 
 // create connection to our database, pass in your MySQL information for username and password
 const sequelize = new Sequelize(process.env.dbNAME, process.env.dbUSER, process.env.dbPASS, {
-    host: 'localhost',
-    dialect: 'mysql',
-    dialectOptions: {
-      decimalNumbers: true,
-    },
-    port: 3306
+  host: 'localhost',
+  dialect: 'mysql',
+  dialectOptions: {
+    decimalNumbers: true,
+  },
+  port: 3306
 });
 
 module.exports = sequelize;
+
